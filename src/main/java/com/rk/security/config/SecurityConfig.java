a75abdfd532c8a6faf8481016250763628f4628d
Radha Krishna <rkanugonda969@gmail.com>
2024-11-10 19:13:21 +0530

    Role Based Auth

--- src/main/java/com/rk/security/config/SecurityConfig.java
+++ src/main/java/com/rk/security/config/SecurityConfig.java
@@ -2,15 +2,21 @@ package com.rk.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -19,4 +25,12 @@ public class SecurityConfig {
                 .httpBasic (withDefaults())
                 .build ();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername ( "admin" ).password( "{noop}admin" ).roles("USER", "ADMIN").build();
+        UserDetails user = User.withUsername ( "user" ).password( "{noop}user" ).roles("USER").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
--- src/main/java/com/rk/security/controller/WishController.java
+++ src/main/java/com/rk/security/controller/WishController.java
@@ -1,12 +1,21 @@
 package com.rk.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WishController {
-    @GetMapping(("/hello"))
-    public String sayHello() {
-        return "Hello";
+
+    @PreAuthorize ( "hasRole('ADMIN')" )
+    @GetMapping(("/admin/hello"))
+    public String sayHelloToAdmin() {
+        return "Hello admin";
+    }
+
+    @PreAuthorize ( "hasRole('USER')" )
+    @GetMapping(("/user/hello"))
+    public String sayHelloToUser() {
+        return "Hello user";
     }
 }
